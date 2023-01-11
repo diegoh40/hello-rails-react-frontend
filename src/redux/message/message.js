d66c29e17c8a1161b8e3d92d165a604e3ede544b
Diego <godie8a@hotmail.com>
2023-01-11 12:19:33 -0500

    fixed error greeting in rendered

--- src/redux/message/message.js
+++ src/redux/message/message.js
@@ -1,15 +1,10 @@
-const GET_MESSAGE_API = 'messageReducer/GET_MESSAGE_API';
+const GET_MESSAGE_API = 'GET_MESSAGE_API';
 
 const initialState = [];
 
-// const getMessage = (payload) => ();
-
 export const getMessageAPI = () => async (dispatch) => {
-  const res = await fetch('http://localhost:3000/greetings/show', {
-    mode: 'no-cors',
-  });
+  const res = await fetch('http://localhost:3000/greetings/show');
   const data = await res.json();
-
   dispatch({
     type: GET_MESSAGE_API,
     data,
@@ -19,7 +14,7 @@ export const getMessageAPI = () => async (dispatch) => {
 const messageReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_MESSAGE_API:
-      return action.payload;
+      return action.data;
     default:
       return state;
   }
